0afe59acf77224f41b0ec3cbaeea0fc1686f0a8f
Angdwina Smith <angdwina@Angdwinas-MacBook-Pro.local>
2025-04-07 05:57:03 +0100

    Retry: fixing permission issue

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
 // firebase-config.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR-KEY",
-  authDomain: "YOUR.firebaseapp.com",
-  projectId: "YOUR-ID",
-  storageBucket: "YOUR.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "APP-ID"
+  apiKey: "AIzaSyBhmQPpQliV41ZH0cXofUsAWo1elsAvD2I",
+  authDomain: "energy-dashboard-angdwina.firebaseapp.com",
+  projectId: "energy-dashboard-angdwina",
+  storageBucket: "energy-dashboard-angdwina.appspot.com",
+  messagingSenderId: "446248966763",
+  appId: "1:446248966763:web:a67c811f6456b17be02f2e"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { db, auth };
+export { app, auth, db };
